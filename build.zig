95fc46af6367360c790ffe17cd97b7b840001664
Bilal <bjlegend007@gmail.com>
2025-04-15 20:22:27 +0530

    added some variable for VM and updated build.zig file

--- build.zig
+++ build.zig
@@ -3,16 +3,8 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
 
-    const exe = b.addExecutable(.{
-        .name = "zbytepusher",
-        .root_module = exe_mod,
-    });
+    const exe = b.addExecutable(.{ .name = "zbytepusher", .root_source_file = b.path("src/main.zig"), .optimize = optimize, .target = target });
 
     b.installArtifact(exe);
 
@@ -40,9 +32,8 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
+    const exe_unit_tests = b.addTest(.{ .root_source_file = b.path("src/main.zig") });
+    exe_unit_tests.root_module.addImport("raylib", raylib);
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,53 @@
-// window size
-const WIDTH: u8 = 256;
-const HEIGHT: u8 = 256;
+// window config
+const WIDTH = 256;
+const HEIGHT = 256;
+const VIDEO_BUFF_SIZE = WIDTH * HEIGHT;
+const TITLE: [:0]const u8 = "Hello BytePusher";
+const FPS = 60;
 
-// memory
-var memory: u8[0 .. 16 * 1024] = undefined;
+// VM config
+const MEMORY_SIZE = 0x1000008;
+const KEY_MEM_SIZE = 16;
+const COLOR_STEP = 0x33;
+const COLOR_BLACK: rl.Color = .{ .r = 0, .b = 0, .g = 0, .a = 255 };
 
-pub fn main() !void {}
+var memory: [MEMORY_SIZE]u8 = undefined;
+var keyMem: [KEY_MEM_SIZE]u8 = undefined;
+var videoBuff: [VIDEO_BUFF_SIZE]u8 = undefined;
+
+const color_map = initColorMap();
+
+fn initColorMap() [256]rl.Color {
+    var c: [256]rl.Color = undefined;
+    for (0..6) |r| for (0..6) |g| for (0..6) |b| {
+        c[r * 36 + g * 6 + b] = .{ .r = 0, .g = 0, .b = 0, .a = 255 };
+    };
+    @memset(c[216..], COLOR_BLACK);
+    return c;
+}
+
+fn init() void {}
+
+fn update() void {
+    for (0..65535) |_| {}
+}
+
+fn draw() void {
+    rl.beginDrawing();
+    defer rl.endDrawing();
+}
+
+pub fn main() !void {
+    rl.initWindow(WIDTH, HEIGHT, TITLE);
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) {
+        update();
+        draw();
+    }
+}
 
 const std = @import("std");
 const rl = @import("raylib");
