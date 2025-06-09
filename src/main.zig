21c746bb137502140cfb775c89cfb53d7c78e159
Bilal <bjlegend007@gmail.com>
2025-06-09 20:03:06 +0530

    small update to zon and main file

--- src/main.zig
+++ src/main.zig
@@ -7,69 +7,11 @@ const Flags = struct {
     file: []const u8,
 };
 
-// window config
-const WIDTH = 256;
-const HEIGHT = 256;
-const VIDEO_BUFF_SIZE = WIDTH * HEIGHT;
-const TITLE: []const u8 = "Hello BytePusher";
-const FPS = 60;
 
-// VM config
-const MEMORY_SIZE = 0x1000008;
-const KEY_MEM_SIZE = 16;
-const COLOR_STEP = 0x33;
-const COLOR_BLACK: rl.Color = .{ .r = 0, .b = 0, .g = 0, .a = 255 };
+fn update() void {}
 
-var memory: [MEMORY_SIZE]u8 = undefined;
-var keyMem: [KEY_MEM_SIZE]u8 = undefined;
-var videoBuff: [VIDEO_BUFF_SIZE]u8 = undefined;
+fn draw() void {}
 
-const color_map = initColorMap();
-
-const CPU = struct {
-    program_counter: u32,
-    memory: [MEMORY_SIZE]u8,
-
-    fn loadRom(location: []const u8) void {
-        _ = location;
-    }
-    fn readByte(location: u32) void {
-        _ = location;
-    }
-
-    fn readDisplayMemory(location: u32) void {
-        _ = location;
-    }
-
-    fn resetProgramCounter() void {}
-
-    fn step() void {}
-
-    fn write(location: u32, data: u8) void {
-        _ = location;
-        _ = data;
-    }
-};
-
-fn initColorMap() [256]rl.Color {
-    var c: [256]rl.Color = undefined;
-    for (0..6) |r| for (0..6) |g| for (0..6) |b| {
-        c[r * 36 + g * 6 + b] = .{ .r = 0, .g = 0, .b = 0, .a = 255 };
-    };
-    @memset(c[216..], COLOR_BLACK);
-    return c;
-}
-
-fn init() void {}
-
-fn update() void {
-    for (0..65535) |_| {}
-}
-
-fn draw() void {
-    rl.beginDrawing();
-    defer rl.endDrawing();
-}
 
 pub fn main() !u8 {
     const cwd = std.fs.cwd();
@@ -85,8 +27,6 @@ pub fn main() !u8 {
     std.debug.print("{any}", .{file});
 
     std.posix.exit(0);
-    rl.initWindow(WIDTH, HEIGHT, TITLE);
-    defer rl.closeWindow();
 
     rl.setTargetFPS(60);
 
