c3b76d77ecde327caac86c5a92c8d70ef6c7da5a
Bilal <bjlegend007@gmail.com>
2025-06-27 20:12:15 +0530

    updated to make it to not to bubble up the errors

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,4 @@
+
 //command line flags
 const Flags = struct {
     pub const description =
@@ -12,24 +13,24 @@ fn update() void {}
 
 fn draw() void {}
 
-
-pub fn main() !u8 {
+pub fn main() u8 {
     const cwd = std.fs.cwd();
 
-    const args = try std.process.argsAlloc(std.heap.smp_allocator);
+    const args = std.process.argsAlloc(std.heap.smp_allocator) catch |err| {
+        std.debug.print("ERROR : found error while allocating argument : {}", .{err});
+        std.posix.exit(1);
+    };
     defer std.process.argsFree(std.heap.smp_allocator, args);
 
     const options = flags.parseOrExit(args, "zbytepusher", Flags, .{});
     const file = cwd.openFile(options.file, .{ .mode = .read_only }) catch |err| {
-        std.debug.print("found error while opening file : {}", .{err});
-        std.posix.exit(0);
+        std.debug.print("ERROR : found error while opening file : {}", .{err});
+        std.posix.exit(1);
     };
     std.debug.print("{any}", .{file});
 
     std.posix.exit(0);
-
     rl.setTargetFPS(60);
-
     while (!rl.windowShouldClose()) {
         update();
         draw();
